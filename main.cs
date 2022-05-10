d7c21deff3aad4e6b16b30b4c32d327f3a0e75a4
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-10 22:48:25 +0000

    4 files

--- main.cs
+++ main.cs
@@ -1,7 +1,42 @@
+// Created by: Marshall Demars
+// Created on: May 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This program tells uses a loop for multiplication
+        int integerOne;
+        int integerTwo;
+
+        // input
+        Console.WriteLine("This program uses a loop for multiplication");
+        Console.WriteLine("");
+        Console.WriteLine("You will need to input 2 integers to be used");
+        Console.WriteLine("");
+        Console.WriteLine("Please input the first integer:");
+        integerOne = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Please input the second integer:");
+        integerTwo = Convert.ToInt32(Console.ReadLine());
+
+        // process
+        Console.WriteLine("");
+        int counter = 0;
+        var answer = 0;
+        while (counter < integerTwo)
+        {
+            Console.WriteLine("Once through loop:" + counter);
+            answer = answer + integerOne;
+            counter++;
+        }
+        Console.WriteLine("The number is " + answer);
+        Console.WriteLine("");
+        Console.WriteLine("\nDone.");
+    }
 }
